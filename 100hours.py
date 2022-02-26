9582445603007329dd3d9200de7a0f02ca6f13f4
Jonny Spicer <spicerjonny@gmail.com>
2022-02-26 17:37:10 +0000

    init 100 hours notion integration

---
+++ 100hours.py
@@ -0,0 +1,48 @@
+import requests
+import json
+import time
+import os
+
+now_epoch = int(time.time())
+start_epoch = 1640995200 # 00:00:00 1st Jan 2022
+end_epoch = 1672531199 # 23:59:59 31st Dec 2022
+
+percentage_through_year = (now_epoch - start_epoch) / (end_epoch - start_epoch) * 100
+
+database_id = os.environ.get('NOTION_DATABASE_ID')
+api_key = os.environ.get('NOTION_API_KEY')
+
+url = f'https://api.notion.com/v1/databases/{database_id}/query'
+
+payload = {"page_size": 100}
+headers = {
+    "Accept": "application/json",
+    "Notion-Version": "2022-02-22",
+    "Content-Type": "application/json",
+    "Authorization": api_key
+}
+
+response = requests.request("POST", url, json=payload, headers=headers)
+
+goal_data = json.loads(response.text)
+values = [percentage_through_year]
+# this is gross and I'm sorry
+values.append(goal_data["results"][0]["properties"]["Duration (Minutes)"]["rollup"]["number"])
+values.append(goal_data["results"][0]["properties"]["Completion"]["formula"]["number"] * 100)
+values.append(goal_data["results"][1]["properties"]["Duration (Minutes)"]["rollup"]["number"])
+values.append(goal_data["results"][1]["properties"]["Completion"]["formula"]["number"] * 100)
+values.append(goal_data["results"][2]["properties"]["Duration (Minutes)"]["rollup"]["number"])
+values.append(goal_data["results"][2]["properties"]["Completion"]["formula"]["number"] * 100)
+values.append(goal_data["results"][3]["properties"]["Duration (Minutes)"]["rollup"]["number"])
+values.append(goal_data["results"][3]["properties"]["Completion"]["formula"]["number"] * 100)
+values.append(goal_data["results"][4]["properties"]["Duration (Minutes)"]["rollup"]["number"])
+values.append(goal_data["results"][4]["properties"]["Completion"]["formula"]["number"] * 100)
+values.append(goal_data["results"][5]["properties"]["Duration (Minutes)"]["rollup"]["number"])
+values.append(goal_data["results"][5]["properties"]["Completion"]["formula"]["number"] * 100)
+
+with open('content/100-hours.md', 'r') as page:
+    pageText = page.read()
+    pageText = pageText.replace(
+        'hours: []', f'hours: {values}')
+with open('content/100-hours.md', 'w') as page:
+    page.write(pageText)
--- content/100-hours.md
+++ content/100-hours.md
@@ -5,6 +5,7 @@ title: "100 Hours"
 description: "My 2022 goal tracker; 6 activities I want to spend 100 hours doing for my health"
 menu: "main"
 weight: "4"
+hours: []
 ---
 There are 8766 hours in a year, and I want to use them efficiently in order to get healthier and happier. To that end I came up with six
 activities that I wanted to spend at least 100 hours doing in 2022, which you can read more about [here.](/blog/2022-goals) This page
